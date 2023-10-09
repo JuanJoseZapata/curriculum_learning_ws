08b5f61e5c2e0e3948832b60abca28c05a56530f
Juan <juanjosezapatagonzalez@hotmail.com>
2023-10-09 15:09:42 +0200

    Normalize rewards

--- scripts/training_cleanrl.py
+++ scripts/training_cleanrl.py
@@ -125,6 +125,76 @@ def parse_args():
     return args
 
 
+# taken from https://github.com/openai/baselines/blob/master/baselines/common/vec_env/vec_normalize.py
+class RunningMeanStd(object):
+    def __init__(self, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, 'float64')
+        self.var = np.ones(shape, 'float64')
+        self.count = epsilon
+
+    def update(self, x):
+        batch_mean = np.mean([x], axis=0)
+        batch_var = np.var([x], axis=0)
+        batch_count = 1
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        self.mean, self.var, self.count = update_mean_var_count_from_moments(
+            self.mean, self.var, self.count, batch_mean, batch_var, batch_count)
+
+def update_mean_var_count_from_moments(mean, var, count, batch_mean, batch_var, batch_count):
+    delta = batch_mean - mean
+    tot_count = count + batch_count
+
+    new_mean = mean + delta * batch_count / tot_count
+    m_a = var * count
+    m_b = batch_var * batch_count
+    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
+    new_var = M2 / tot_count
+    new_count = tot_count
+
+    return new_mean, new_var, new_count
+
+
+class NormalizedEnv(gym.core.Wrapper):
+    def __init__(self, env, ob=False, ret=True, clipob=3., cliprew=3., gamma=0.99, epsilon=1e-8):
+        super(NormalizedEnv, self).__init__(env)
+        self.ob_rms = RunningMeanStd(shape=self.observation_space.shape) if ob else None
+        self.ret_rms = RunningMeanStd(shape=(1,)) if ret else None
+        self.clipob = clipob
+        self.cliprew = cliprew
+        self.ret = np.zeros(())
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+    def step(self, action):
+        obs, rews, dones, truncations, infos = self.env.step(action)
+        for agent_id in range(len(infos)):
+            infos[agent_id]['episode']['real_reward'] = rews[agent_id]
+        self.ret = self.ret * self.gamma + rews
+        obs = self._obfilt(obs)
+        if self.ret_rms:
+            self.ret_rms.update(np.array([self.ret].copy()))
+            rews = np.clip(rews / np.sqrt(self.ret_rms.var + self.epsilon), -self.cliprew, self.cliprew)
+
+        self.ret = self.ret * (1-np.array(dones).astype(float))
+
+        return obs, rews, dones, truncations, infos
+
+    def _obfilt(self, obs):
+        if self.ob_rms:
+            self.ob_rms.update(obs)
+            obs = np.clip((obs - self.ob_rms.mean) / np.sqrt(self.ob_rms.var + self.epsilon), -self.clipob, self.clipob)
+            return obs
+        else:
+            return obs
+
+    def reset(self, seed=None, options=None):
+        self.ret = np.zeros(())
+        obs, infos = self.env.reset()
+        return self._obfilt(obs), infos
+
+
 def make_env():
 
     # env setup
@@ -147,6 +217,8 @@ def make_env():
         env.render_mode = None
         env = ss.clip_reward_v0(env, lower_bound=-3, upper_bound=3)
     env = ss.pettingzoo_env_to_vec_env_v1(env)
+    # Normalize rewards
+    env = NormalizedEnv(env)
 
     return env
 
