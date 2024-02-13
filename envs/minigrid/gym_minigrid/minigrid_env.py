2f0cc00c0d83566a77808a537fd7e4b51ed1c26d
JuanJoseZapata <juanjosezapatagonzalez@hotmail.com>
2024-02-13 12:38:52 +0100

    Update MiniGrid env for training

--- envs/minigrid/gym_minigrid/minigrid_env.py
+++ envs/minigrid/gym_minigrid/minigrid_env.py
@@ -26,6 +26,7 @@ class Env(MiniGridEnv):
         self.width = width
         self.height = height
         self.num_tiles = num_tiles
+        self.num_envs = 1
 
         mission_space = MissionSpace(mission_func=self._gen_mission)
 
@@ -146,9 +147,84 @@ class Env(MiniGridEnv):
 
         return obs, {}
 
+    def step(
+        self, action: ActType
+    ) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        
+        self.step_count += 1
+
+        reward = 0
+        terminated = False
+        truncated = False
+
+        # Get the position in front of the agent
+        fwd_pos = self.front_pos
+
+        # Get the contents of the cell in front of the agent
+        fwd_cell = self.grid.get(*fwd_pos)
+
+        # Rotate left
+        if action == self.actions.left:
+            self.agent_dir -= 1
+            if self.agent_dir < 0:
+                self.agent_dir += 4
+
+        # Rotate right
+        elif action == self.actions.right:
+            self.agent_dir = (self.agent_dir + 1) % 4
+
+        # Move forward
+        elif action == self.actions.forward:
+            if fwd_cell is None or fwd_cell.can_overlap():
+                self.agent_pos = tuple(fwd_pos)
+            if fwd_cell is not None and fwd_cell.type == "goal":
+                terminated = True
+                reward = self._reward()
+            if fwd_cell is not None and fwd_cell.type == "lava":
+                terminated = True
+
+        # Pick up an object
+        elif action == self.actions.pickup:
+            if fwd_cell and fwd_cell.can_pickup():
+                if self.carrying is None:
+                    self.carrying = fwd_cell
+                    self.carrying.cur_pos = np.array([-1, -1])
+                    self.grid.set(fwd_pos[0], fwd_pos[1], None)
+
+        # Drop an object
+        elif action == self.actions.drop:
+            if not fwd_cell and self.carrying:
+                self.grid.set(fwd_pos[0], fwd_pos[1], self.carrying)
+                self.carrying.cur_pos = fwd_pos
+                self.carrying = None
+
+        # Toggle/activate an object
+        elif action == self.actions.toggle:
+            if fwd_cell:
+                fwd_cell.toggle(self, fwd_pos)
+
+        # Done action (not used by default)
+        elif action == self.actions.done:
+            pass
+
+        else:
+            raise ValueError(f"Unknown action: {action}")
+
+        if self.step_count >= self.max_steps:
+            truncated = True
+
+        if self.render_mode == "human":
+            self.render()
+
+        obs = self.gen_obs()
+
+        info = [{'r': reward, 'l': self.step_count, 't': terminated, 'truncated': truncated}]
+
+        return obs, reward, terminated, truncated, info
+
 
 def main():
-    env = SimpleEnv(render_mode="human")
+    env = Env(render_mode="human")
 
     # enable manual control for testing
     manual_control = ManualControl(env, seed=42)
